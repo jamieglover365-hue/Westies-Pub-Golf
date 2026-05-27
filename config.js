558360e47f0050f3ed6beb9192f7983a00376784
jamieglover365-hue <jamieglover365@gmail.com>
2026-05-27 21:54:18 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.PUB_GOLF_CONFIG = {
   supabaseUrl: "https://uznemjzscepugooeanee.supabase.co",
-  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXJhYmFzZSIsInJlZiI6InV6bmVtanpzY2VwdWdvb2VhbmVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5MDQwNjQsImV4cCI6MjA5NTQ4MDA2NH0.NcEBGDQ0Ywo_lP5jyDowi4O0l62A1FrCE2WX0IbGBZU",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6bmVtanpzY2VwdWdvb2VhbmVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5MDQwNjQsImV4cCI6MjA5NTQ4MDA2NH0.NcEBGDQ0Ywo_lP5jyDowi4O0l62A1FrCE2WX0IbGBZU",
   adminPassword: "Westies1"
 };
